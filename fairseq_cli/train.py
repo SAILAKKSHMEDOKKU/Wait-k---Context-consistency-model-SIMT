b52e996a9cc1f60180179c8c9bb352a1ba1ffe13
Kruthi kaveri <sreekruthikaveri@gmail.com>
2025-03-31 21:17:13 +0530

    updated train.py

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -65,7 +65,8 @@ def main(args, init_distributed=False):
 
     # Load valid dataset (we load training data below, based on the latest checkpoint)
     for valid_sub_split in args.valid_subset.split(','):
-        task.load_dataset(valid_sub_split, combine=False, epoch=1)
+            task.load_dataset(valid_sub_split, combine=False, epoch=1)
+            dataset.truncate(max_tokens=25000)
 
     # Build model and criterion
     model = task.build_model(args)
